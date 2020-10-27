efd331218f41e91a13a0eef26c96127dcc77cbc1
jwambeek <70319876+jwambeek@users.noreply.github.com>
2020-10-27 11:27:07 +0530

    modules

---
+++ modules/sh_product_multi_barcode/models/product_barcode.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) Softhealer Technologies.
+
+from odoo import models, fields, api
+
+
+class ShProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    barcode_line_ids = fields.One2many(
+        related='product_variant_ids.barcode_line_ids', readonly=False)
+
+
+class ShProduct(models.Model):
+    _inherit = 'product.product'
+
+    barcode_line_ids = fields.One2many(
+        'product.template.barcode', 'product_id', 'Barcode Lines')
+
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        res = super(ShProduct, self)._name_search(name=name, args=args,
+                                                  operator=operator, limit=limit, name_get_uid=name_get_uid)
+        mutli_barcode_search = self._search(
+            [('barcode_line_ids', '=', name)] + args, limit=limit, access_rights_uid=name_get_uid)
+        multi_barcode_res = []
+        if mutli_barcode_search:
+            multi_barcode_res = self.browse(mutli_barcode_search).name_get()
+        return res + multi_barcode_res
+
+
+class ShProductBarcode(models.Model):
+    _name = 'product.template.barcode'
+    _description = "Product Barcode"
+
+    product_id = fields.Many2one('product.product', 'Product')
+    name = fields.Char("Barcode", required=True)

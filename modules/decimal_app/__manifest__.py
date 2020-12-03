285350af7649a9b7f797d6a7a1c570d69f00ca2a
jwambeek <70319876+jwambeek@users.noreply.github.com>
2020-12-03 13:41:10 +0530

    first update

---
+++ modules/decimal_app/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name' : 'Deciaml',
+    'version': '1.3',
+    'Summary': 'Change decimal places',
+    'description': 'To print the new report',
+    'license': 'LGPL-3',
+    'depends': [
+        'account_accountant'
+    ],    
+    'installable': True,
+    'application':True,
+    'auto_install':False
+}
